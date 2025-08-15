b72450dfd4660cd265dce32b2602716dfc465fb2
vijaykumarpallerla <pallerlavijay315@gmail.com>
2025-08-15 07:55:56 +0530

    25346

--- orders/views.py
+++ orders/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'homepage.html')
+
+
+def custom_404(request, exception):
+    return render(request , '404.html', status=404)
